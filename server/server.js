bd48fb163f8a56efbb34ff7d96d9211fcd05b360
Brent Jure <brentjure@gmail.com>
2022-11-24 10:58:32 +0300

    connected to the database

--- server/server.js
+++ server/server.js
@@ -1,7 +1,25 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+process.on('UncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION. Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: 'config.env' });
 const app = require('./app');
 
-const port = 8000;
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
+mongoose.connect(DB).then(() => {
+  console.log(`DB connection successful`);
+});
+
+const port = process.env.PORT;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
