305c13f9958d4c6e6cc10984429c943f923d1949
Brent Jure <brentjure@gmail.com>
2022-12-12 23:07:45 +0300

    implemented booking model

---
+++ server/contollers/bookingController.js
@@ -0,0 +1,8 @@
+const factory = require('./handlerFactory');
+const Booking = require('../Models/bookingModel');
+
+exports.createBooking = factory.createOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
---
+++ server/routes/bookingRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bookingController = require('../contollers/bookingController');
+const authController = require('../contollers/authController');
+
+const router = express.Router();
+
+router.use(authController.protect);
+
+router.use(authController.restrictTo('admin', 'lead-guide'));
+
+router
+  .route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking);
+
+router
+  .route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
+
+module.exports = router;
