d24eca2e1c11ade936ce122a11b4d2d455d9442a
Brent Jure <brentjure@gmail.com>
2022-12-12 10:57:31 +0300

    prevented duplicate reviews

--- server/Models/reviewModel.js
+++ server/Models/reviewModel.js
@@ -32,6 +32,8 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.statics.calcAverageRatings = async function (tourId) {
   const stats = await this.aggregate([
     {
