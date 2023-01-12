65ae9f2483484a35bad2825ea4b78461a10f6ed9
Brent Jure <brentjure@gmail.com>
2023-01-12 10:45:51 +0300

    fixed url not found error

--- server/app.js
+++ server/app.js
@@ -66,7 +66,7 @@ app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
 app.use('/api/v1/bookings', bookingRouter);
 
-app.use('*', (req, res, next) => {
+app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
